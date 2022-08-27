de4205b455f096d715d0fab473f55558b42e1ba7
Asmina <asmina@keyvalue.systems>
2022-08-27 18:32:01 +0530

    commit

--- app/assets/images/index.js
+++ app/assets/images/index.js
@@ -1,6 +1,1 @@
-export const currentPosition = require('./blue-dot.png');
-export const search = require('./search.png');
-export const stationIcon = require('./e-black.png');
-export const login = require('./login.png');
-export const stationPreview = require('./station.png');
-export const success = require('./success.png');
+export const login = require('./login.png')
