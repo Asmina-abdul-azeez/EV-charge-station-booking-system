e6e4e106bcd1ab2fe8925d45c1fd3a1aab25d2d1
Rose Mary Abraham <rosemary@keyvalue.systems>
2022-08-28 05:47:24 +0530

    Adding Profile screen

--- app/screens/Feed/components/Header.js
+++ app/screens/Feed/components/Header.js
@@ -3,11 +3,12 @@ import {Image, Text, View} from 'react-native';
 import {burger, notification, plus} from '../../../assets/appImages';
 import {fonts} from '../../../utilities/Constants';
 
-const Header = () => (
+// eslint-disable-next-line react/prop-types
+const Header = ({title}) => (
   <View style={{paddingHorizontal: 18, flexDirection: 'row', alignItems: 'center', backgroundColor: 'white', justifyContent: 'space-between', paddingVertical: 8, borderBottomWidth: 1, borderBottomColor: '#E6E6E6'}}>
     <View style={{flexDirection: 'row', alignItems: 'center'}}>
       <Image source={burger} style={{width: 24, height: 24}} />
-      <Text style={{paddingLeft: 20, fontSize: 20, color: '#1A1A1A', fontFamily: fonts.PRIMARY_FONT_BOLD, textAlignVertical: 'center'}}>Feeds</Text>
+      <Text style={{paddingLeft: 20, fontSize: 20, color: '#1A1A1A', fontFamily: fonts.PRIMARY_FONT_BOLD, textAlignVertical: 'center'}}>{title || 'Feeds'}</Text>
     </View>
     <View style={{flexDirection: 'row', alignItems: 'flex-end'}}>
       <Image source={plus} style={{width: 24, height: 24}} />
